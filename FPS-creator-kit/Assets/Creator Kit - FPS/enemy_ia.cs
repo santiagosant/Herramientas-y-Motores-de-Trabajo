eaac04587e9a9a64bd99c755baf49ae6c6068a32
ChaosTomatoe <santidevargas@gmail.com>
2022-07-09 15:55:28 -0300

    [UPD] Los enemigos te ven y persiguen

---
+++ FPS-creator-kit/Assets/Creator Kit - FPS/enemy_ia.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy_ia : MonoBehaviour
+{
+
+    public float range_alert;
+    public float speed;
+    public LayerMask player_layer;
+    public Transform player;
+    bool is_in_range;
+
+    void Update()
+    {
+        is_in_range = Physics.CheckSphere(transform.position, range_alert, player_layer);
+
+
+        if (is_in_range == true)
+        {
+            Vector3 see_and_go = new Vector3(player.position.x, transform.position.y, player.position.z);
+            transform.LookAt(see_and_go);
+            transform.position = Vector3.MoveTowards(transform.position, see_and_go, speed * Time.deltaTime);
+        }
+
+    }
+
+}
